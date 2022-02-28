29d9870064498a4addfc22aec52c10bdd67b62af
trungthien2001 <82750730+trungthien2001@users.noreply.github.com>
2022-02-27 18:29:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 /*
 CSC3916 HW2
+Jeremy Gage
 File: Server.js
-Description: Web API scaffolding for Movie API
+Description: Web API for Movie API
  */
 
 var express = require('express');
@@ -41,37 +42,47 @@ function getJSONObjectForMovieRequirement(req) {
     return json;
 }
 
-router.post('/signup', function(req, res) {
-    if (!req.body.username || !req.body.password) {
-        res.json({success: false, msg: 'Please include both username and password to signup.'})
-    } else {
-        var newUser = {
-            username: req.body.username,
-            password: req.body.password
-        };
+router.route('/signup')
+    .post( function(req, res) {
+        if (!req.body.username || !req.body.password) {
+            res.json({success: false, msg: 'Please include both username and password to signup.'})
+        } else {
+            var newUser = {
+                username: req.body.username,
+                password: req.body.password
+            };
 
-        db.save(newUser); //no duplicate checking
-        res.json({success: true, msg: 'Successfully created new user.'})
-    }
-});
-
-router.post('/signin', function (req, res) {
-    var user = db.findOne(req.body.username);
-
-    if (!user) {
-        res.status(401).send({success: false, msg: 'Authentication failed. User not found.'});
-    } else {
-        if (req.body.password == user.password) {
-            var userToken = { id: user.id, username: user.username };
-            var token = jwt.sign(userToken, process.env.SECRET_KEY);
-            res.json ({success: true, token: 'JWT ' + token});
+            db.save(newUser); //no duplicate checking
+            res.json({success: true, msg: 'Successfully created new user.'})
         }
-        else {
-            res.status(401).send({success: false, msg: 'Authentication failed.'});
-        }
+    }).all(function (req, res){
+        res.status(405).send({success: false, msg: 'HTTP method not implemented.'})
     }
-});
+);
 
+router.route('/signin')
+    .post(function (req, res) {
+            var user = db.findOne(req.body.username);
+
+            if (!user) {
+                res.status(401).send({success: false, msg: 'Authentication failed. User not found.'});
+            } else {
+                if (req.body.password === user.password) {
+                    var userToken = { id: user.id, username: user.username };
+                    var token = jwt.sign(userToken, process.env.SECRET_KEY);
+                    res.json ({success: true, token: 'JWT ' + token});
+                }
+                else {
+                    res.status(401).send({success: false, msg: 'Authentication failed.'});
+                }
+            }
+        }
+    ).all(function (req, res){
+        res.status(405).send({success: false, msg: 'HTTP method not implemented.'})
+    }
+);
+
+/*
 router.route('/testcollection')
     .delete(authController.isAuthenticated, function(req, res) {
         console.log(req.body);
@@ -80,6 +91,7 @@ router.route('/testcollection')
             res = res.type(req.get('Content-Type'));
         }
         var o = getJSONObjectForMovieRequirement(req);
+        o.body={msg:"movie deleted."}
         res.json(o);
     }
     )
@@ -90,12 +102,62 @@ router.route('/testcollection')
             res = res.type(req.get('Content-Type'));
         }
         var o = getJSONObjectForMovieRequirement(req);
+            o.body={msg:"movie updated."}
         res.json(o);
     }
     );
+*/
+
+//implement movie route
+router.route('/movies')
+    .get(authJwtController.isAuthenticated, function(req, res) {
+        console.log(req.body);
+        res = res.status(200);
+        if (req.get('Content-Type')) {
+            res = res.type(req.get('Content-Type'));
+        }
+        var o = getJSONObjectForMovieRequirement(req);
+        o.body={msg:"GET movies."}
+        res.json(o);
+
+    }).put(authJwtController.isAuthenticated, function(req, res) {
+        console.log(req.body);
+        res = res.status(200);
+        if (req.get('Content-Type')) {
+            res = res.type(req.get('Content-Type'));
+        }
+        var o = getJSONObjectForMovieRequirement(req);
+        o.body={msg:"movie updated."}
+        res.json(o);
+    }
+).delete(authController.isAuthenticated, function(req, res) {
+        console.log(req.body);
+        res = res.status(200);
+        if (req.get('Content-Type')) {
+            res = res.type(req.get('Content-Type'));
+        }
+        var o = getJSONObjectForMovieRequirement(req);
+
+        o.body={msg:"movie deleted."}
+        res.json(o);
+    }
+).post(authJwtController.isAuthenticated, function(req, res) {
+        console.log(req.body);
+        res = res.status(200);
+        if (req.get('Content-Type')) {
+            res = res.type(req.get('Content-Type'));
+        }
+        var o = getJSONObjectForMovieRequirement(req);
+        o.body={msg:"movie saved."}
+        res.json(o);
+    }
+).all(function (req, res){
+        res.status(405).send({success: false, msg: 'HTTP method not implemented.'})
+    }
+);
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
-
+module.exports = app; // for testing only
 
 
